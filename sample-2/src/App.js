f47b6cb4d3677b6ed5182988aea0d27f6ec88aee
alagu-p <alagappan.plr@gmail.com>
2022-11-10 22:09:05 +0530

    Day-60

--- sample-2/src/App.js
+++ sample-2/src/App.js
@@ -1,18 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello World
+//         </p>
         
-      </header>
-    </div>
-  );
-}
+//       </header>
+//     </div>
+//   );
+// }
+const App=(a,b)=><p>{10+20}</p>
 
 export default App;
