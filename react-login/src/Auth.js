3aa7351e973bedf155dbeb335a34498fd3c91495
alagu-p <alagappan.plr@gmail.com>
2022-11-11 22:56:48 +0530

    Day-61

---
+++ react-login/src/Auth.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function(props){
+    return (
+        <div className="Auth-form-container">
+            <form className="Auth-form">
+                <div></div>
+            </form>
+        </div>
+
+    );
+}
