f819a05d741e1714a6e3be597d9c6907342a7b59
azhagu379 <a.palanisamy@memob.com>
2022-11-11 14:28:02 +0530

    Day-61

---
+++ login-page/src/Auth.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function(props){
+    return (<div className="Auth-form-container ">
+        <form className="Auth-form">
+            <div className="Auth-form-content">
+                <h3 className="Auth-form-title"> Sign In</h3>
+                <div className="form-group mt-3">
+                    <label>Email Address</label>
+                    <input/>
+                </div>
+            </div>
+        </form>
+
+    </div>);
+}
