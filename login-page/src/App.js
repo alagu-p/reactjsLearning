e2cf7285225ea6d058223b3e2f17be352f9e5d40
azhagu379 <a.palanisamy@memob.com>
2022-11-11 19:10:42 +0530

    sample theme

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -8,7 +8,7 @@ function App() {
   
     <BrowserRouter>
     <Routes>
-      <Route path="/auth" element={(<Auth/>)} />
+      <Route path="/" element={(<Auth/>)} />
     </Routes>
     </BrowserRouter>
   );
