7011dc4edb7a5e0f973411dbdeb19c1df0c00cc4
azhagu379 <a.palanisamy@memob.com>
2022-11-11 15:09:03 +0530

    Day-61

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Auth from "./Auth";
 
 function App() {
   return (
--- login-page/src/Auth.js
+++ login-page/src/Auth.js
@@ -1,13 +1,28 @@
 import React from "react"
 
 export default function(props){
-    return (<div className="Auth-form-container ">
+    return (<div className="Auth-form-container">
         <form className="Auth-form">
             <div className="Auth-form-content">
                 <h3 className="Auth-form-title"> Sign In</h3>
+
                 <div className="form-group mt-3">
-                    <label>Email Address</label>
-                    <input/>
+                    <input type="email" className="form-control mt-1"
+                    placeholder="Your email"/>
+                </div>
+
+                <div className="form-group mt-3">
+                    <input type="password" className="form-control mt-1"
+                    placeholder="Password"/>
+                </div>
+                <div className="d-grid gap-2 mt-3">
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                    </button>
+                    <p className="forgot-password text-right mt-2">
+                        <a href="#">Forgot password?</a>
+                    </p>
+
                 </div>
             </div>
         </form>
